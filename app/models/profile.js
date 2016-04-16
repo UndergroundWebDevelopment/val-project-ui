0045731fad2e1e1e8ac2baf3614ebbf299dfca8a
Alex Willemsma <alex@undergroundwebdevelopment.com>
2016-04-16 15:08:20 -0400

    Support for dob, profile public description.

--- app/models/profile.js
+++ app/models/profile.js
@@ -2,8 +2,12 @@ import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  firstName: DS.attr(),
-  lastName: DS.attr(),
+  firstName: DS.attr('string'),
+  lastName: DS.attr('string'),
+  slug: DS.attr('string'),
+  publicDescription: DS.attr('string'),
+  dateOfBirth: DS.attr('date'),
+  publicDateOfBirth: DS.attr('boolean'),
 
   name: Ember.computed('firstName', 'lastName', function() {
     return `${this.get('firstName')} ${this.get('lastName')}`;
